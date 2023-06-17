abaf96967a8b21e0ba3b51427c7b3e2092d62821
luomolhx <2932965369@qq.com>
2023-06-18 01:30:50 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const str = '我爱你'
+console.log(str)
+</script>
+
+<template>
+  <div class="box">
+    <h1>我是APP根组件</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
